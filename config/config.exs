d653689456d5e7392cf9f0a2a0664a7bbefa2f9f
Dustin McCraw <dustinmccraw@yahoo.com>
2016-07-20 21:23:43 -0700

    add config

--- config/config.exs
+++ config/config.exs
@@ -27,10 +27,11 @@ use Mix.Config
 # Configuration from the imported file will override the ones defined
 # here (which is why it is important to import them last).
 #
-# import_config "#{Mix.env}.exs"
 config :hans_and_franz, slack_token: System.get_env("HANS_AND_FRANZ_SLACK_TOKEN")
 
 config :hans_and_franz,
   default_timezone:   "America/Denver", # timezone used
   office_hour_start:  9, # hour that messages should start
   office_hour_end:    16 # hour that messages should end
+
+import_config "#{Mix.env}.exs"
